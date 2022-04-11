960af8de4fee76398498765442c93ad5bdbac2aa
Honorable con <honorablecon@gmail.com>
2022-04-11 06:30:24 +0000

    Ajouter les requetes pour créer les élements du dashboard

--- app/Http/Controllers/ContratController.php
+++ app/Http/Controllers/ContratController.php
@@ -4,9 +4,60 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Contrat;
+use DateTime;
 
 class ContratController extends Controller
 {
+    public function getInterimaireEffectifsForYear($year){
+        $contrats = Contrat::whereYear('debut', $year)->where('type_contrat_id', 4);
+        $data = [
+            "janvier" => $contrats->whereMonth('debut', '01')->get()->count(),
+            "fevrier" => $contrats->whereMonth('debut', '02')->get()->count(),
+            "mars" => $contrats->whereMonth('debut', '03')->get()->count(),
+            "avril" => $contrats->whereMonth('debut', '04')->get()->count(),
+            "mai" => $contrats->whereMonth('debut', '05')->get()->count(),
+            "juin" => $contrats->whereMonth('debut', '06')->get()->count(),
+            "juillet" => $contrats->whereMonth('debut', '07')->get()->count(),
+            "aout" => $contrats->whereMonth('debut', '08')->get()->count(),
+            "septembre" => $contrats->whereMonth('debut', '09')->get()->count(),
+            "octobre" => $contrats->whereMonth('debut', '10')->get()->count(),
+            "novembre" => $contrats->whereMonth('debut', '11')->get()->count(),
+            "decembre" => $contrats->whereMonth('debut', '12')->get()->count(),
+            "Total" => $contrats->get()->count(),
+        ];
+
+        return response()->json($data);
+    }
+
+    public function getInterimaireEffectifsForYearAndMonth($year, $monthNumber){
+        $monthName = DateTime::createFromFormat('!m', $monthNumber)->format('F');
+        $contrats = Contrat::whereYear('debut', $year)
+            ->where('type_contrat_id', 4)
+            ->whereMonth('debut', $monthNumber);
+        $data = [
+            $monthName => $contrats->whereMonth('debut', $monthNumber)->get()->count(),
+        ];
+
+        return response()->json($data);
+    }
+
+    public function getPermanentEffectifsForYear($year){
+        $contrats = Contrat::whereYear('debut', $year)->get();
+
+        $cdd = $contrats->where('type_contrat_id', 1)->count();
+        $cdi = $contrats->where('type_contrat_id', 2)->count();
+        $stage = $contrats->where('type_contrat_id', 3)->count();
+
+        $data = [
+            "year" => $year,
+            "CDD" => $cdd,
+            "CDI" => $cdi,
+            "Stage" => $stage,
+            "Total" => $cdd + $cdi + $stage,
+        ];
+
+        return response()->json($data);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -58,7 +109,7 @@ class ContratController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($contrat)
     {
         return Contrat::destroy($contrat);
     }
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,14 @@
 <?php
 
-use App\Models\AllContrant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonnelController;
+use App\Http\Controllers\ContratController;
 use App\Http\Controllers\CauseController;
 use App\Http\Controllers\DepartController;
 use App\Http\Controllers\StatutController;
-use App\Http\Controllers\ContratController;
 use App\Http\Controllers\FiliereController;
 use App\Http\Controllers\FonctionController;
-use App\Http\Controllers\PersonnelController;
 use App\Http\Controllers\StructureController;
 use App\Http\Controllers\AllContrantController;
 /*
@@ -27,14 +26,63 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/allcontrats', [AllContrantController::class, 'index']);
-Route::resource('causes', CauseController::class);
+Route::get('/allcontrats', [PersonnelController::class, 'personnels']);
+Route::get('/permanents/{year}', [ContratController::class, 'getPermanentEffectifsForYear']);
+Route::get('/interimaires/{year}', [ContratController::class, 'getInterimaireEffectifsForYear']);
+Route::get('/interimaires/{year}/{month}', 
+    [ContratController::class, 'getInterimaireEffectifsForYearAndMonth']
+);
 Route::resource('contrats', ContratController::class);
-Route::resource('departs', DepartController::class);
-Route::resource('filieres', FiliereController::class);
-Route::resource('fonctions', FonctionController::class);
 Route::resource('personnels', PersonnelController::class);
-Route::resource('statuts', StatutController::class);
-Route::resource('structures', StructureController::class);
-Route::resource('typeContrats', TypeContratController::class);
 Route::resource('users', UserController::class);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Route::get('/allcontrats', [AllContrantController::class, 'index']);
+// Route::resource('causes', CauseController::class);
+// Route::resource('departs', DepartController::class);
+// Route::resource('filieres', FiliereController::class);
+// Route::resource('fonctions', FonctionController::class);
+// Route::resource('statuts', StatutController::class);
+// Route::resource('structures', StructureController::class);
+// Route::resource('typeContrats', TypeContratController::class);
